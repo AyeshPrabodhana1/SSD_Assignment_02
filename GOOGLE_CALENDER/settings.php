49b6761659900fbc5f5ab71025e654ae5fc54c15
ayesh <ayesh1prabodhana@gmail.com>
2021-09-29 20:44:25 +0530

    SSD2_DONE

---
+++ GOOGLE_CALENDER/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '511793529915-3ausuirufau53bloc0usbfll3omo02v3.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'xGQYTLW9wtkRGBsZKwh6X4hn');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/SSD_Assignment_02/GOOGLE_CALENDER/google-login.php');
+
+?>
